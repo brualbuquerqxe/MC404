a94e37bc9b9bcb7e2f03237e5a902df97e895d45
balbuquerqxe <brunadaher@icloud.com>
2025-09-12 03:10:09 -0300

    Funcao que extrai o número

--- Labs/lab6/lab6a.s
+++ Labs/lab6/lab6a.s
@@ -2,10 +2,10 @@
 
 .section .bss
 
-# Buffer: aloca 21 bytes, já que é o tamanho máximo da entrada (conta a pulada de linha final).
+# Buffer: aloca 20 bytes, já que é o tamanho máximo da entrada (conta a pulada de linha final).
 
 buffer:
-    .skip 21
+    .skip 20
 
 # Prontooo! Agora temos o buffer (área em que vou guardar temporariamente a minha entrada) alocado. Isso signfica que eu posso sair da seção .bss.
 
@@ -22,7 +22,7 @@ buffer:
 leitura:
     li   a0, 0  # STDIN = 0 (entrada padrão)
     la   a1, buffer # Buffer onde a entrada será armazenada
-    li   a2, 21 # Número máximo de bytes a serem lidos
+    li   a2, 20 # Número máximo de bytes a serem lidos
     li   a7, 63 # Código do serviço de leitura
     ecall # Chamada de sistema
 
@@ -39,6 +39,56 @@ escrita:
     mv   a2, t0 # Move o valor de t0 (número de bytes lidos) para o registrador a2
     li   a7, 64 # Código do serviço de escrita
     ecall # Chamada de sistema
+
+# A diretiva ".globl extracao_numero" transforma o símbolo "extracao_numero" em global, ou seja, ele pode ser acessado por outros arquivos.
+
+.globl extracao_numero
+
+# A função "extracao_numero" extrai o número da string armazenada no buffer e o converte para um valor inteiro.
+
+extracao_numero:
+
+    li   a0, 0 # Inicializa o registrador a0 com 0 (onde o número final será armazenado)
+
+    lbu t0, 0(a1) # Leitura do primeiro caractere da string (coloca em um registrador temporário)
+    addi t0, t0, -'0' # Converte o caractere ASCII para um inteiro
+    mul t0, t0, 1000 # Multiplica o valor por 1000 (primeiro dígito)
+    add a0, a0, t0 # Adiciona o valor ao registrador a0(soma)
+
+    lbu t0, 1(a1) # Leitura do segundo caractere da string (coloca em um registrador temporário)
+    addi t0, t0, -'0' # Converte o caractere ASCII para um inteiro
+    mul t0, t0, 100 # Multiplica o valor por 100 (segundo dígito)
+    add a0, a0, t0 # Adiciona o valor ao registrador a0(soma)
+
+    lbu t0, 2(a1) # Leitura do terceiro caractere da string (coloca em um registrador temporário)
+    addi t0, t0, -'0' # Converte o caractere ASCII para um inteiro
+    mul t0, t0, 10 # Multiplica o valor por 10 (terceiro dígito)
+    add a0, a0, t0 # Adiciona o valor ao registrador a0(soma)
+
+    lbu t0, 3(a1) # Leitura do quarto caractere da string (coloca em um registrador temporário)
+    addi t0, t0, -'0' # Converte o caractere ASCII para um inteiro
+    mul t0, t0, 1 # Multiplica o valor por 1 (quarto dígito)
+    add a0, a0, t0 # Adiciona o valor ao registrador a0(soma)
+
+    ret # Retorna da função, sendo a0 o lugar que vai estar armazenado o número final
+
+# A diretiva ".globl raiz_quadrada" transforma o símbolo "raiz_quadrada" em global, ou seja, ele pode ser acessado por outros arquivos.
+
+.globl raiz_quadrada
+
+# A função "raiz_quadrada" calcula a raiz quadrada do número armazenado em a0 e retorna o resultado em a0.
+
+raiz_quadrada:
+    
+
+
+
+
+
+
+
+
+
 
 # A diretiva ".globl main" transforma o símbolo "main" em global, ou seja, ele pode ser acessado por outros arquivos.
 
