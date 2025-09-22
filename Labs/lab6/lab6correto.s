7e591cc27c2615d879c21b400f2814fdce2d5f54
balbuquerqxe <brunadaher@icloud.com>
2025-09-22 14:30:49 -0300

    Adiciona lógica de comparar as raízes

--- Labs/lab6/lab6correto.s
+++ Labs/lab6/lab6correto.s
@@ -38,6 +38,8 @@ saida:
 
 	.globl   numero_sinal
 
+	.globl   define_x
+
 # Inicia o programa a partir do rótulo "_start".
 _start:
 	call     main
@@ -170,6 +172,51 @@ negativo:
 	mul      a0, a0, t0                    # Multiplica o número por -1
 	ret
 
+# Função que escolhe o melhor valor de X
+define_x:
+# Define Y*Y e DC*DC
+	mul      t0, s3, s3                    # Y*Y == t0
+	mul      t1, s8, s8                    # DC*DC == t1
+
+# Diferença X com o sinal original
+	sub      t2, s10, s11                  # (X - X_c)
+	mul      t2, t2, t2                    # (X - X_c)*(X - X_c)
+	add      t2, t2, t0                    #(X - X_c)*(X - X_c) + Y*Y
+	sub      t2, t2, t1                    #(X - X_c)*(X - X_c) + Y*Y - DC*DC
+	bge      t2, x0, diferenca_positivaf   # Se a diferença for maior ou igual x0
+	blt      t2, x0 diferenca_negativaf    # Se a diferença for menor que x0
+	mv       s11, t3                       # Move resultado de t3 para s11
+
+# Diferença X com o sinal oposto ao original
+	li       t4, -1
+	mul      t4, s10, t4                   # Multiplica por -1
+	sub      t2, t4, s11                   # (X - X_c)
+	mul      t2, t2, t2                    # (X - X_c)*(X - X_c)
+	add      t2, t2, t0                    #(X - X_c)*(X - X_c) + Y*Y
+	sub      t2, t2, t1                    #(X - X_c)*(X - X_c) + Y*Y - DC*DC
+	bge      t2, x0, diferenca_positivaf   # Se a diferença for maior ou igual x0
+	blt      t2, x0 diferenca_negativaf    # Se a diferença for menor que x0
+
+# Compara os dois valores (se s11 < t3, X == s10 e, caso contrário, X == t4. A partir daí, define X == s10).
+	blt      t3, s11, melhor_xf
+	ret
+
+# Se a diferença for positiva, armazena em t3 ela mesmo!
+diferenca_positiva:
+	mv       t3, t2                        # t3 = Diferença sinal original
+	ret
+
+# Se a diferença for negativa, armazena em t3 o valor invertido!
+diferenca_negativa:
+	sub      t3, x0, t2                    # Se for menor, faz a diferença ficar positiva
+	ret
+
+# O melhor X é com o valor contrário ao que estava
+melhor_x:
+	mv       s10, t4                       # Coloca o valor de X multiplicado por -1
+	ret
+
+
 # Função principal do programa
 main:
 # Constantes
@@ -244,7 +291,7 @@ main:
 	li       s4, -1                        # -1 para subtrações
 	li       s5, 2                         # 2 para multiplicações
 
-# Y = (DA*DA + Y_b*Y_b - (DB*DB))/(2*Yb)
+# Y == (DA*DA + Y_b*Y_b - (DB*DB))/(2*Yb) == s3
 	mul      s6, s6, s6                    # DA*DA (usa depois)
 	mv       s3, s6                        # Move para s3
 	mul      s2, s10, s10                  # Y_b*Y_b
@@ -255,14 +302,13 @@ main:
 	mul      s2, s10, s5                   # 2*Y_b
 	div      s3, s3, s2                    # Y == s3
 
-# X = +-raiz(DA*DA - (Y*Y))
+# X = +-raiz(DA*DA - (Y*Y)) == s10
 	mul      s7, s3, s3                    # Y*Y
 	mul      s7, s7, s4                    # -(Y*Y)
-	add      s10, s7, s6                   # dA^2 - y^2 (em s10)
+	add      s10, s7, s6                   # DA*DA - Y*Y (em s10)
 	mv       a0, s10                       # Move para a0
 	call     raiz_quadrada                 # Calcula raiz quadrada
-	mv       s10, a0                       # Move devolta para s10
-
+	mv       s10, a0                       # Move de volta para s10
 
 # Imprime
 	call     imprime_saida_yx
