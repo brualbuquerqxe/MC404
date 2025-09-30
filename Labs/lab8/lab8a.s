73054a401443db7891e608b82b1982f991a5354e
balbuquerqxe <brunadaher@icloud.com>
2025-09-30 18:02:20 -0300

    Terminei a lógica do código mas nn roda

--- Labs/lab8/lab8a.s
+++ Labs/lab8/lab8a.s
@@ -76,8 +76,8 @@ extracaoLarguraCabecalho:
 	addi     t4, a1, 3                  # Endereço do buffer
 	li       t5, 3                      # 3 Dígitos
 for1:
-	addi     t4, t4, 1                  # Endereço do caractere atual
 	lbu      t0, 0(t4)                  # Leitura do primeiro caractere da string
+	addi     t4, t4, 1                  # Endereço do caractere atual
 	addi     t0, t0, -'0'               # Converte o caractere ASCII para um inteiro
 	mul      t0, t0, t2                 # Multiplica o valor pelo multiplicador
 	add      s1, t0, s1                 # Armazena a largura
@@ -87,6 +87,7 @@ for1:
 	j        for1                       # Continua o loop, caso contrário
 cont1:
 	mv       s2, s1                     # Largura = s2
+	ret
 
 extracaoAlturaCabecalho:
 # Altura da imagem
@@ -97,8 +98,8 @@ extracaoAlturaCabecalho:
 	addi     t4, a1, 7                  # Endereço do buffer
 	li       t5, 3                      # 3 Dígitos
 for2:
-	addi     t4, t4, 1                  # Endereço do caractere atual
 	lbu      t0, 0(t4)                  # Leitura do primeiro caractere da string
+	addi     t4, t4, 1                  # Endereço do caractere atual
 	addi     t0, t0, -'0'               # Converte o caractere ASCII para um inteiro
 	mul      t0, t0, t2                 # Multiplica o valor pelo multiplicador
 	add      s1, t0, s1                 # Armazena a largura
@@ -108,6 +109,7 @@ for2:
 	j        for2                       # Continua o loop, caso contrário
 cont2:
 	mv       s3, s1                     # Altura = s3
+	ret
 
 extracaoMaxCinzaCabecalho:
 # Máximo do cinza da imagem
@@ -118,8 +120,8 @@ extracaoMaxCinzaCabecalho:
 	addi     t4, a1, 11                 # Endereço do buffer
 	li       t5, 3                      # 3 Dígitos
 for3:
-	addi     t4, t4, 1                  # Endereço do caractere atual
 	lbu      t0, 0(t4)                  # Leitura do primeiro caractere da string
+	addi     t4, t4, 1                  # Endereço do caractere atual
 	addi     t0, t0, -'0'               # Converte o caractere ASCII para um inteiro
 	mul      t0, t0, t2                 # Multiplica o valor pelo multiplicador
 	add      s1, t0, s1                 # Armazena a largura
@@ -129,7 +131,7 @@ for3:
 	j        for3                       # Continua o loop, caso contrário
 cont3:
 	mv       s4, s1                     # MaxVal = s4
-
+	ret
 
 
 # Definição da cor
@@ -144,6 +146,7 @@ defineCorPixel:
 	slli     s7, s5, 8                  # Left Shift do B
 	add      s6, s6, s7                 # Soma R + G + B
 	addi     s6, s6, 255                # Finaliza somando A (Cor do Pixel = s6)
+	ret
 
 # Possibilita a visualização da imagem
 criacaoImagem:
@@ -153,28 +156,34 @@ criacaoImagem:
 
 	li       t3, 0                      # Começo com o contador da posição no buffer!
 
-	mul      t4, s2, s3                 # O máximo de bytes da imagem (não é o máximo possível, se não vou ler lixo)!
+	li      t4, s2, s3                 # O máximo de bytes da imagem (não é o máximo possível, se não vou ler lixo)!
 
 	mv       t5, s2                     # O tamanho da linha
 
 # Função que passa por todos os bytes que formam a imagem
 passagemPelaImagem:
-	li       s5, 0                      # Redefino a posição no buffer para 0!
+
+	beqz     t3, pula                   # Pula se t3 = 0, já que, mesmo o resto sendo zero, continua na mesma linha!
 
 	rem      t6, t3, t5                 # Calcula o resto da posição pelo comprimento da linha!
 
 	beqz     t6, novaLinha              # Se o resto for igual a zero, significa que estamos em uma nova linha!
 
-	j        mesmaLinha                 # Se o resto for diferente de zero, seguimos na mesma linha
+	call     mesmaLinha                 # Se o resto for diferente de zero, seguimos na mesma linha
 
-	add      s5, a1, t3                 # Uso s5 para armazenar a posição no buffer do caracter que estou lendo (a1 = buffer)
+pula:
 
-	j        setPixel                   # Define a cor de cada Pixel
+	add      t0, a1, t3                 # Endereço = buffer + índice = t0
+	lbu      s5, 0(t0)                  # s5 = Valor do cinza
+
+	call     defineCorPixel
+
+	call     setPixel                   # Define a cor de cada Pixel
 
 	addi     t3, t3, 1                  # Desloco uma pro lado no buffer (próximo Pixel)
 
-	bge      t4, t3, passagemPelaImagem # Se ainda não li todos os bytes, volto para a função.
-
+	blt      t3, t4, passagemPelaImagem # Se ainda não li todos os bytes, volto para a função.
+	ret
 
 # Passa para uma nova linha!
 novaLinha:
@@ -182,12 +191,13 @@ novaLinha:
 	li       t1, 0                      # Voltamos para a primeira coluna (X = 0)
 
 	addi     t2, t2, 1                  # Descemos mais uma linha (Y = Y + 1)
-
+	j        pula                       # Pula o mesma Linha
 
 # Continuo na mesma linha
 mesmaLinha:
 
 	addi     t1, t1, 1                  # Avançamos na largura da linha (X = X + 1)
+	ret
 
 # Leitura da imagem
 leituraImagem:
@@ -203,6 +213,7 @@ close:
 	mv       a0, s0                     # Identificador da imagem
 	li       a7, 57                     # syscall close
 	ecall
+	ret
 
 
 # Define a cor de cada Pixel!
@@ -212,18 +223,23 @@ setPixel:
 	mv       a2, s6                     # Cor do Pixel = s6
 	li       a7, 2200                   # syscall setPixel (2200)
 	ecall
+	ret
 
 # Redefine o tamanho da tela
 setCanvasSize:
 	mv       a0, s2                     # Largura da tela
 	mv       a1, s3                     # Altura da tela
 	li       a7, 2201                   # syscall setCanvasSize (2201)
+	ecall
+	ret
 
 # Inspeção da imagem
 setScaling:
 	li       a0, 1
 	li       a1, 1
 	li       a7, 2202
+	ecall
+	ret
 
 # Função principal
 main:
@@ -239,6 +255,12 @@ main:
 	call     extracaoAlturaCabecalho    # Altura = s3
 	call     extracaoMaxCinzaCabecalho  # Max = s4
 
+# Define o Canvas
+	call     setCanvasSize
+
+# Define escala
+	call setScaling
+
 # Leitura da imagem
 	mul      s5, s2, s3                 # Quantidade de Pixels (preciso fazer isso para não ficar esperando entrada)
 	addi     s5, s5, 1                  # Já que temos o último espaço em branco
@@ -247,9 +269,6 @@ main:
 
 # Criação da imagem!
 	call     criacaoImagem
-
-# Define o Canvas
-	call     setCanvasSize
 
 # Inspeção
 	call     close
