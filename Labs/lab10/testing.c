9897f35fa34868777bc53612374c67b71c869fa1
balbuquerqxe <brunadaher@icloud.com>
2025-10-15 16:04:11 -0300

    Passou em tudo, glória

---
+++ Labs/lab10/testing.c
@@ -0,0 +1,215 @@
+
+    #include "lib.h"
+
+    char buffer[100];
+    int number = 2882;
+
+    #define NULL 0
+
+    void run_operation(int op){
+        int val;
+        Node node_45,node_41,node_25,node_46,node_63,node_21,node_131,node_79,node_33,node_100,node_74,node_26,node_32,node_50,node_124,node_71,node_155,node_44,node_15,node_87,node_7,node_36,node_58,node_102,node_39,node_68,node_90,node_78,node_154,node_119,node_114,node_70,node_57,node_64,node_94,node_16,node_143,node_136,node_52,node_72,node_20,node_145,node_77,node_135,node_144,node_89,node_29,node_121,node_142,node_43,node_11,node_157,node_22,node_110,node_47,node_56,node_61,node_137,node_88,node_66,node_101,node_4,node_6,node_5,node_23,node_118,node_104,node_24,node_80,node_59,node_98,node_151,node_27,node_2,node_86,node_53,node_34,node_49,node_40,node_55,node_67,node_149,node_140,node_19,node_134,node_130,node_127,node_150,node_93,node_109,node_35,node_48,node_113,node_105,node_139,node_17,node_42,node_91,node_126,node_9,node_116,node_18,node_111,node_122,node_96,node_132,node_13,node_141,node_128,node_95,node_99,node_112,node_97,node_83,node_69,node_108,node_37,node_158,node_161,node_160,node_133,node_147,node_31,node_73,node_125,node_76,node_75,node_107,head_node,node_82,node_148,node_81,node_51,node_103,node_1,node_152,node_138,node_123,node_28,node_3,node_92,node_65,node_30,node_115,node_85,node_117,node_129,node_153,node_38,node_54,node_159,node_146,node_12,node_8,node_156,node_84,node_60,node_10,node_14,node_62,node_106,node_120;
+        head_node.val1 = 10;head_node.val2 = -4;head_node.next = &node_1;
+        node_1.val1 = 56;node_1.val2 = 78;node_1.next = &node_2;
+        node_2.val1 = -654;node_2.val2 = 590;node_2.next = &node_3;
+        node_3.val1 = -100;node_3.val2 = -43;node_3.next = &node_4;
+        node_4.val1 = 521;node_4.val2 = 8687;node_4.next = &node_5;
+        node_5.val1 = 5578;node_5.val2 = 2731;node_5.next = &node_6;
+        node_6.val1 = -172;node_6.val2 = 7124;node_6.next = &node_7;
+        node_7.val1 = -8078;node_7.val2 = 6727;node_7.next = &node_8;
+        node_8.val1 = -9006;node_8.val2 = 4715;node_8.next = &node_9;
+        node_9.val1 = -6164;node_9.val2 = 2730;node_9.next = &node_10;
+        node_10.val1 = -3969;node_10.val2 = 2512;node_10.next = &node_11;
+        node_11.val1 = 3261;node_11.val2 = -12513;node_11.next = &node_12;
+        node_12.val1 = 7883;node_12.val2 = 125;node_12.next = &node_13;
+        node_13.val1 = -5582;node_13.val2 = 3303;node_13.next = &node_14;
+        node_14.val1 = -9327;node_14.val2 = 13524;node_14.next = &node_15;
+        node_15.val1 = -3964;node_15.val2 = 6886;node_15.next = &node_16;
+        node_16.val1 = 8623;node_16.val2 = -14178;node_16.next = &node_17;
+        node_17.val1 = -4777;node_17.val2 = -952;node_17.next = &node_18;
+        node_18.val1 = 9798;node_18.val2 = -1897;node_18.next = &node_19;
+        node_19.val1 = -7623;node_19.val2 = 15738;node_19.next = &node_20;
+        node_20.val1 = -532;node_20.val2 = 9669;node_20.next = &node_21;
+        node_21.val1 = -9718;node_21.val2 = 5306;node_21.next = &node_22;
+        node_22.val1 = -4345;node_22.val2 = 6504;node_22.next = &node_23;
+        node_23.val1 = 2946;node_23.val2 = 5846;node_23.next = &node_24;
+        node_24.val1 = -2176;node_24.val2 = -6942;node_24.next = &node_25;
+        node_25.val1 = 158;node_25.val2 = 9419;node_25.next = &node_26;
+        node_26.val1 = -1291;node_26.val2 = 6896;node_26.next = &node_27;
+        node_27.val1 = 3945;node_27.val2 = -6194;node_27.next = &node_28;
+        node_28.val1 = 4066;node_28.val2 = -11941;node_28.next = &node_29;
+        node_29.val1 = -6766;node_29.val2 = 6609;node_29.next = &node_30;
+        node_30.val1 = -5686;node_30.val2 = 11318;node_30.next = &node_31;
+        node_31.val1 = -2951;node_31.val2 = -6240;node_31.next = &node_32;
+        node_32.val1 = -1758;node_32.val2 = -6759;node_32.next = &node_33;
+        node_33.val1 = -5412;node_33.val2 = 821;node_33.next = &node_34;
+        node_34.val1 = 8915;node_34.val2 = -2188;node_34.next = &node_35;
+        node_35.val1 = 5717;node_35.val2 = -9207;node_35.next = &node_36;
+        node_36.val1 = -4723;node_36.val2 = 5822;node_36.next = &node_37;
+        node_37.val1 = 5570;node_37.val2 = -1981;node_37.next = &node_38;
+        node_38.val1 = -417;node_38.val2 = 8808;node_38.next = &node_39;
+        node_39.val1 = 705;node_39.val2 = -7809;node_39.next = &node_40;
+        node_40.val1 = -4934;node_40.val2 = -2179;node_40.next = &node_41;
+        node_41.val1 = 922;node_41.val2 = -5337;node_41.next = &node_42;
+        node_42.val1 = -4227;node_42.val2 = 139;node_42.next = &node_43;
+        node_43.val1 = 6592;node_43.val2 = 3103;node_43.next = &node_44;
+        node_44.val1 = 980;node_44.val2 = 3145;node_44.next = &node_45;
+        node_45.val1 = 6150;node_45.val2 = -6959;node_45.next = &node_46;
+        node_46.val1 = -3508;node_46.val2 = -2514;node_46.next = &node_47;
+        node_47.val1 = -9862;node_47.val2 = 11621;node_47.next = &node_48;
+        node_48.val1 = -2792;node_48.val2 = 2075;node_48.next = &node_49;
+        node_49.val1 = 551;node_49.val2 = -6146;node_49.next = &node_50;
+        node_50.val1 = -2421;node_50.val2 = -5717;node_50.next = &node_51;
+        node_51.val1 = -4192;node_51.val2 = -2864;node_51.next = &node_52;
+        node_52.val1 = 5794;node_52.val2 = -6761;node_52.next = &node_53;
+        node_53.val1 = 2200;node_53.val2 = -3377;node_53.next = &node_54;
+        node_54.val1 = -2461;node_54.val2 = -4878;node_54.next = &node_55;
+        node_55.val1 = -3440;node_55.val2 = 3644;node_55.next = &node_56;
+        node_56.val1 = 4756;node_56.val2 = -10043;node_56.next = &node_57;
+        node_57.val1 = -9714;node_57.val2 = 4564;node_57.next = &node_58;
+        node_58.val1 = 5801;node_58.val2 = -13637;node_58.next = &node_59;
+        node_59.val1 = -5062;node_59.val2 = 6528;node_59.next = &node_60;
+        node_60.val1 = -7176;node_60.val2 = 2129;node_60.next = &node_61;
+        node_61.val1 = 8965;node_61.val2 = -314;node_61.next = &node_62;
+        node_62.val1 = -2372;node_62.val2 = 1332;node_62.next = &node_63;
+        node_63.val1 = 7487;node_63.val2 = -8919;node_63.next = &node_64;
+        node_64.val1 = 5393;node_64.val2 = -11083;node_64.next = &node_65;
+        node_65.val1 = -2617;node_65.val2 = -378;node_65.next = &node_66;
+        node_66.val1 = 7356;node_66.val2 = -11999;node_66.next = &node_67;
+        node_67.val1 = -1883;node_67.val2 = -6897;node_67.next = &node_68;
+        node_68.val1 = 7308;node_68.val2 = -9990;node_68.next = &node_69;
+        node_69.val1 = -1272;node_69.val2 = -2985;node_69.next = &node_70;
+        node_70.val1 = 2123;node_70.val2 = 4766;node_70.next = &node_71;
+        node_71.val1 = -9940;node_71.val2 = 15965;node_71.next = &node_72;
+        node_72.val1 = 6515;node_72.val2 = 882;node_72.next = &node_73;
+        node_73.val1 = -6863;node_73.val2 = 1630;node_73.next = &node_74;
+        node_74.val1 = -6026;node_74.val2 = 5332;node_74.next = &node_75;
+        node_75.val1 = 4581;node_75.val2 = -11863;node_75.next = &node_76;
+        node_76.val1 = 133;node_76.val2 = 6506;node_76.next = &node_77;
+        node_77.val1 = 7395;node_77.val2 = -16677;node_77.next = &node_78;
+        node_78.val1 = -5963;node_78.val2 = 5384;node_78.next = &node_79;
+        node_79.val1 = 2409;node_79.val2 = 1466;node_79.next = &node_80;
+        node_80.val1 = -4268;node_80.val2 = 6132;node_80.next = &node_81;
+        node_81.val1 = -2450;node_81.val2 = -2016;node_81.next = &node_82;
+        node_82.val1 = 4886;node_82.val2 = -2652;node_82.next = &node_83;
+        node_83.val1 = -1839;node_83.val2 = -2223;node_83.next = &node_84;
+        node_84.val1 = -7799;node_84.val2 = 8270;node_84.next = &node_85;
+        node_85.val1 = 1224;node_85.val2 = 2941;node_85.next = &node_86;
+        node_86.val1 = -1579;node_86.val2 = 9693;node_86.next = &node_87;
+        node_87.val1 = 8141;node_87.val2 = -138;node_87.next = &node_88;
+        node_88.val1 = -8941;node_88.val2 = 12320;node_88.next = &node_89;
+        node_89.val1 = 9394;node_89.val2 = -3951;node_89.next = &node_90;
+        node_90.val1 = 5714;node_90.val2 = 1395;node_90.next = &node_91;
+        node_91.val1 = 6508;node_91.val2 = -13696;node_91.next = &node_92;
+        node_92.val1 = -5803;node_92.val2 = 13617;node_92.next = &node_93;
+        node_93.val1 = 9932;node_93.val2 = -17995;node_93.next = &node_94;
+        node_94.val1 = 9525;node_94.val2 = -8424;node_94.next = &node_95;
+        node_95.val1 = -9999;node_95.val2 = 9179;node_95.next = &node_96;
+        node_96.val1 = 9131;node_96.val2 = -1960;node_96.next = &node_97;
+        node_97.val1 = 3301;node_97.val2 = -12689;node_97.next = &node_98;
+        node_98.val1 = 7597;node_98.val2 = 2268;node_98.next = &node_99;
+        node_99.val1 = 5169;node_99.val2 = -10245;node_99.next = &node_100;
+        node_100.val1 = 8987;node_100.val2 = -16044;node_100.next = &node_101;
+        node_101.val1 = 2829;node_101.val2 = -2948;node_101.next = &node_102;
+        node_102.val1 = 9677;node_102.val2 = -2816;node_102.next = &node_103;
+        node_103.val1 = 9634;node_103.val2 = -5188;node_103.next = &node_104;
+        node_104.val1 = -9447;node_104.val2 = 12543;node_104.next = &node_105;
+        node_105.val1 = -569;node_105.val2 = -5777;node_105.next = &node_106;
+        node_106.val1 = 2558;node_106.val2 = -8555;node_106.next = &node_107;
+        node_107.val1 = 3995;node_107.val2 = -8832;node_107.next = &node_108;
+        node_108.val1 = -2739;node_108.val2 = -5265;node_108.next = &node_109;
+        node_109.val1 = 497;node_109.val2 = -8015;node_109.next = &node_110;
+        node_110.val1 = 2324;node_110.val2 = 994;node_110.next = &node_111;
+        node_111.val1 = 9572;node_111.val2 = -319;node_111.next = &node_112;
+        node_112.val1 = 9525;node_112.val2 = -15202;node_112.next = &node_113;
+        node_113.val1 = 773;node_113.val2 = -3345;node_113.next = &node_114;
+        node_114.val1 = 5372;node_114.val2 = -6879;node_114.next = &node_115;
+        node_115.val1 = -9626;node_115.val2 = 7687;node_115.next = &node_116;
+        node_116.val1 = -999;node_116.val2 = -6762;node_116.next = &node_117;
+        node_117.val1 = 4369;node_117.val2 = -8460;node_117.next = &node_118;
+        node_118.val1 = -8396;node_118.val2 = 17744;node_118.next = &node_119;
+        node_119.val1 = 5740;node_119.val2 = -11610;node_119.next = &node_120;
+        node_120.val1 = 2168;node_120.val2 = 3353;node_120.next = &node_121;
+        node_121.val1 = -1722;node_121.val2 = 7213;node_121.next = &node_122;
+        node_122.val1 = -7465;node_122.val2 = 15589;node_122.next = &node_123;
+        node_123.val1 = -9891;node_123.val2 = 14701;node_123.next = &node_124;
+        node_124.val1 = -9543;node_124.val2 = 18895;node_124.next = &node_125;
+        node_125.val1 = 8570;node_125.val2 = -8374;node_125.next = &node_126;
+        node_126.val1 = 7982;node_126.val2 = -1023;node_126.next = &node_127;
+        node_127.val1 = -9110;node_127.val2 = 3923;node_127.next = &node_128;
+        node_128.val1 = 7053;node_128.val2 = -4024;node_128.next = &node_129;
+        node_129.val1 = 9389;node_129.val2 = -12390;node_129.next = &node_130;
+        node_130.val1 = -4191;node_130.val2 = 11979;node_130.next = &node_131;
+        node_131.val1 = 5152;node_131.val2 = -2975;node_131.next = &node_132;
+        node_132.val1 = 816;node_132.val2 = -8605;node_132.next = &node_133;
+        node_133.val1 = -6370;node_133.val2 = 841;node_133.next = &node_134;
+        node_134.val1 = -6395;node_134.val2 = -2137;node_134.next = &node_135;
+        node_135.val1 = 3575;node_135.val2 = -11458;node_135.next = &node_136;
+        node_136.val1 = 7455;node_136.val2 = -9408;node_136.next = &node_137;
+        node_137.val1 = -9061;node_137.val2 = 9807;node_137.next = &node_138;
+        node_138.val1 = 3656;node_138.val2 = -10318;node_138.next = &node_139;
+        node_139.val1 = 5281;node_139.val2 = -6646;node_139.next = &node_140;
+        node_140.val1 = 5678;node_140.val2 = 3462;node_140.next = &node_141;
+        node_141.val1 = 8160;node_141.val2 = -16709;node_141.next = &node_142;
+        node_142.val1 = -247;node_142.val2 = 7917;node_142.next = &node_143;
+        node_143.val1 = -2869;node_143.val2 = 1350;node_143.next = &node_144;
+        node_144.val1 = 8852;node_144.val2 = -16065;node_144.next = &node_145;
+        node_145.val1 = 7550;node_145.val2 = -6298;node_145.next = &node_146;
+        node_146.val1 = -8633;node_146.val2 = 6351;node_146.next = &node_147;
+        node_147.val1 = -4979;node_147.val2 = 7871;node_147.next = &node_148;
+        node_148.val1 = 5512;node_148.val2 = -10597;node_148.next = &node_149;
+        node_149.val1 = -1698;node_149.val2 = 2960;node_149.next = &node_150;
+        node_150.val1 = -7617;node_150.val2 = 4438;node_150.next = &node_151;
+        node_151.val1 = 7076;node_151.val2 = -2521;node_151.next = &node_152;
+        node_152.val1 = -2893;node_152.val2 = 7743;node_152.next = &node_153;
+        node_153.val1 = -9140;node_153.val2 = 12639;node_153.next = &node_154;
+        node_154.val1 = -6952;node_154.val2 = 10443;node_154.next = &node_155;
+        node_155.val1 = -3802;node_155.val2 = 4874;node_155.next = &node_156;
+        node_156.val1 = 605;node_156.val2 = -1368;node_156.next = &node_157;
+        node_157.val1 = -7889;node_157.val2 = 15959;node_157.next = &node_158;
+        node_158.val1 = -6427;node_158.val2 = 15434;node_158.next = &node_159;
+        node_159.val1 = -4883;node_159.val2 = 3309;node_159.next = &node_160;
+        node_160.val1 = -3678;node_160.val2 = 2884;node_160.next = &node_161;
+        node_161.val1 = -601;node_161.val2 = -8748;node_161.next = NULL;
+        
+        switch (op){
+            case 0:
+                puts(buffer);
+                break;
+
+            case 1:
+                gets(buffer);
+                puts(buffer);
+                break;
+
+            case 2:
+                puts(itoa(number, buffer, 10));
+                break;
+
+            case 3:
+                puts(itoa(atoi(gets(buffer)), buffer, 16));
+                break;
+
+            case 4:
+                gets(buffer);
+                puts(buffer);
+                gets(buffer);
+                puts(buffer);
+                break;
+
+            case 5:
+                val = atoi(gets(buffer));
+                puts(itoa(linked_list_search(&head_node, val), buffer, 10));
+                break;
+            
+            default:
+                break;
+            }
+    }
+
+    void _start(){
+        int operation = atoi(gets(buffer));
+        run_operation(operation);
+        exit(0);
+    }
+    
