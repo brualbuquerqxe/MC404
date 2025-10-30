716908bcb4a1ac3a4e81b2bc31b87853f5f4e747
balbuquerqxe <brunadaher@icloud.com>
2025-10-30 14:24:02 -0300

    Estruturação da mai e da leitura de casos

--- Labs/lab13/lab13.s
+++ Labs/lab13/lab13.s
@@ -1,6 +1,9 @@
 # Seção para dados não incializados.
 	.section .bss
 
+bufferComando:
+	.skip    3                        # Tamanho do comando
+
 bufferEntrada:
 	.skip    100000                   # Tamanho desconhecido
 
@@ -14,6 +17,27 @@ bufferEntrada:
 
 # Endereço do byte que deve ser lido
 	.set     enderecoLeitura, 0x01
+
+# Função que inicia o programa
+	.globl   _start
+
+# Função que lê a entrada
+	.globl   leituraEntrada
+
+# Função que lida com o primeiro caso
+	.globl   caso01
+
+# Função que lida com o segundo caso
+	.globl   caso02
+
+# Função que lida com o terceiro caso
+	.globl   caso03
+
+# Função que lida com o quarto caso
+	.globl   caso04
+
+# Função principal
+	.globl   main
 
 # Inicia o programa
 _start:
@@ -59,10 +83,27 @@ leituraEntrada:
 # Função principal do programa
 main:
 
-	call     preparaVeiculo
+	la       a5, bufferComando        # Salva o comando
 
-	call     verificaDistancia
+	call     leituraEntrada           # Leitura da entrada até o '\n'
+	lb       s1, 0(a5)                # Valor da instrução
 
-	li       a0, 0                    # Código de saída
-	li       a7, 93                   # Serviço de saída
-	ecall
+	la       a5, bufferEntrada        # Salva a frase/ operação
+
+	call     leituraEntrada           # Leitura da entrada até o '\n'
+
+	mv       a2, t1                   # Quantidades de caracteres lidos
+
+	li       s2, 1                    # Caso 01
+	beq      s1, s2, caso01           # Chama a função do primeiro caso
+
+	li       s2, 2                    # Caso 02
+	beq      s1, s2, caso02           # Chama a função do segundo caso
+
+	li       s2, 3                    # Caso 03
+	beq      s1, s2, caso03           # Chama a função do terceiro caso
+
+	li       s2, 4                    # Caso 04
+	beq      s1, s2, caso04           # Chama a função do quarto caso
+
+	ret                               # Fim!
